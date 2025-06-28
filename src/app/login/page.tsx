ab7fe1b95d6f34128423aab6b4358bb969aead56
AbdelrhmanHisham000 <aboodhisham222@gmail.com>
2025-06-28 14:11:27 +0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,16 @@
-
-
 import SignInButton from "../../_components/SignInButton";
 
 export const metadata = {
   title: "Login | BlackLuxe",
-  description: "Access your account securely. Enter your credentials to log in and manage your dashboard.",
+  description:
+    "Access your account securely. Enter your credentials to log in and manage your dashboard.",
 };
-
 
 export default function Page() {
   return (
-    <div className="flex flex-col gap-10 mt-10 items-center">
-      <h2 className="text-3xl font-semibold">
-        Sign in to access your guest area
+    <div className="mt-10 flex flex-col items-center gap-10">
+      <h2 className="text-center text-3xl font-semibold">
+        Sign in to unlock your exclusive BlackLuxe experience
       </h2>
 
       <SignInButton />
